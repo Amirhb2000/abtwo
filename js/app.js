4853eebb9d6f6dde37e439befaed6a8f82fa85fa
Amirhb2000 <amirbaghestani2000@gmail.com>
2021-06-06 22:08:14 +0430

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// const navslide = () => {
+//     const btn = document.querySelector('#icon');
+//     const nav = document.querySelector('.menu');
+
+//     btn.addEventListener('click', () => {
+//         nav.classList.toggle('nav-active');
+//     });
+// }
+
+// navslide();
